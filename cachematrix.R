391ce7bf0cd34e098be32e3396ef80f469ae3370
HybridRbt <littlejack0120@gmail.com>
2014-05-22 00:47:57 -0500

    add comments

--- cachematrix.R
+++ cachematrix.R
@@ -4,19 +4,23 @@
 ## Write a short comment describing this function
 ## This function creates a special "matrix" object that can cache its inverse.
 ## Will follow the pattern of the example makeVector funtion
+## Note to self: try to understand this syntax as a class definition
 
 makeCacheMatrix <- function(x = matrix()) {
-    i <- NULL
-    set <- function(y) {
+    i <- NULL ## inverse of matrix x
+  
+    set <- function(y) {  ## "constructor"
         x <<- y
         i <<- NULL
     }
-    get <- function() x
+    get <- function() x ## return x
+    
     setinv <- function(solve) i <<- solve
     getinv <- function() i
-    list(set = set, get = get,
-         setinv = setinv,
-         getinv = getinv)
+    
+    ## since the matrix will always be invertible, thus nrow(x) == ncol(x) 
+    ## == nrow(i) == ncol(i) 
+    matrix(c(x, i), nrow = nrow(x), ncol = ncol(x) + ncol(x))  
 }
 
 
