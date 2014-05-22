7d4be8dc153146550dfc4fdd076321c995b74c28
HybridRbt <littlejack0120@gmail.com>
2014-05-21 23:59:51 -0500

    Create makeCacheMatrix funtion
    follow the pattern of the example makeVector function

--- cachematrix.R
+++ cachematrix.R
@@ -2,9 +2,21 @@
 ## functions do
 
 ## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
+## Will follow the pattern of the example makeVector funtion
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL
+    set <- function(y) {
+        x <<- y
+        i <<- NULL
+    }
+    get <- function() x
+    setinv <- function(solve) i <<- solve
+    getinv <- function() i
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
 
